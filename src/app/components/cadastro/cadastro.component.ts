2c71e47d89a995a7e092544a233e6473b45cbd19
Ramon-Mateus <ramonmateus00@gmail.com>
2025-02-06 22:19:31 -0300

    feat: verificação de senha forte com pop up de aviso

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -4,6 +4,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { AutenticacaoService } from '../../services/autenticacao.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,20 +13,34 @@ import { Router } from '@angular/router';
   imports: [
     FormsModule,
     InputTextModule,
-    PasswordModule
+    PasswordModule,
+    CommonModule
   ],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AutenticacaoService, private router: Router) {}
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   registro() {
-    this.authService.registrar({ email: this.email, password: this.password }).subscribe(() => {
-      this.router.navigate(['/login']);
-    });
+    if (this.isPasswordStrong(this.password)) {
+      this.authService.registrar({ email: this.email, password: this.password }).subscribe(() => {
+        this.router.navigate(['/login']);
+      });
+    } else {
+      this.toastr.error('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número, um símbolo e ter no mínimo 8 caracteres', 'Erro de Validação');
+    }
+  }
+
+  isPasswordStrong(password: string): boolean {
+    const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
+    return pattern.test(password);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { LOCALE_ID } from '@angular/core';
+import { importProvidersFrom, LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 registerLocaleData(localePt);
 
@@ -12,5 +14,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     ...(appConfig.providers || []), // Garante que não sobrescrevemos outros providers
     { provide: LOCALE_ID, useValue: 'pt-BR' }, // Adiciona o LOCALE_ID como 'pt-BR'
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(ToastrModule.forRoot())
   ],
 }).catch((err) => console.error(err));
