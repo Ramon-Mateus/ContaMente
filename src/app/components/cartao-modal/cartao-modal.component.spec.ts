cef1d3e7dcce8328b3f4f71db5ab881694553ec0
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2025-08-23 00:55:13 -0300

    feat: implement card management modal with create and edit functionality

---
+++ src/app/components/cartao-modal/cartao-modal.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CartaoModalComponent } from './cartao-modal.component';
+
+describe('CartaoModalComponent', () => {
+  let component: CartaoModalComponent;
+  let fixture: ComponentFixture<CartaoModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CartaoModalComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CartaoModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/components/cartao/cartao.component.ts
+++ src/app/components/cartao/cartao.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Cartao } from '../../lib/types';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ResponsavelService } from '../../services/responsavel.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-cartao',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    CardModule,
+    ConfirmPopupModule,
+    ToastModule,
+    ButtonModule,
+    DialogModule,
+    SidebarModule,
+    FormsModule,
+    InputTextareaModule
+  ],
   templateUrl: './cartao.component.html',
-  styleUrl: './cartao.component.css'
+  styleUrl: './cartao.component.css',
+  providers: [ConfirmationService, MessageService]
 })
 export class CartaoComponent {
+  @Input() cartao!: Cartao;
+  @Output() Delete = new EventEmitter<boolean>();
+  @Output() Edit = new EventEmitter<number>();
 
+  constructor(
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private responsavelService: ResponsavelService
+  ) {}
+ 
+  confirmDelete(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Todas as movimentações associadas a esse cartão também serão excluídas! Tem certeza que deseja excluir esse cartão?',
+      accept: () => {
+        this.onDelete();
+        this.messageService.add({ severity: 'info', summary: 'Confirmado', detail: 'Cartão excluído com sucesso', life: 3000 });
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'error', summary: 'Rejeitado', detail: 'Cartão não excluído', life: 3000 });
+      }
+    });
+  }
+
+  onDelete() {
+    this.responsavelService.deleteResponsavel(this.cartao.id!).subscribe({
+      next: () => {
+        this.Delete.emit(true);
+      }
+    })
+  }
+
+  onEdit() {
+    this.Edit.emit(this.cartao.id);
+  }
 }
