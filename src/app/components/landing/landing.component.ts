dde3118b908a7680f37919f1d39713987804a617
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2025-11-08 11:30:17 -0300

    fix: removido import não utilizado mais

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { Component, inject } from '@angular/core'
-import { Router, RouterLink } from '@angular/router'
+import { Router } from '@angular/router'
 import { AutenticacaoService } from '../../services/autenticacao.service'
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
